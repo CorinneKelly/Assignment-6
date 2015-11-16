b466fc8d057927684374799dd805187b57847157
Corinne Kelly <corinnemariekelly@gmail.com>
2015-11-15 21:23:33 -0500

    Re-submission-fixed syntax

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ $(document).ready(function() {
 	$('h1').animate( {
 		opacity: 0.5,
 	}, 4000);
-}
+
 
 // this logs a message in console when you click the button
-	'#button').click(function() {
+	$('button').click(function() {
 	console.log('You clicked it!');
-}
+	});
 
 // makes all the h2 titles disappear
-	function() {
+	$(function() {
 	$('h2').slideUp(4000);
-}
+	});
 
 // hides all the paragraph text
-	function() {
+	$(function() {
 	$('p').hide();
+	});
 });
